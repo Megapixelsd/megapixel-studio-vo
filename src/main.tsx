e1b87bce3a384620f399f5fe9d555b69971dbc6e
Megapixelsd <megapixelsd@gmail.com>
2026-06-16 14:31:15 +0000

    Force dark background with vanilla CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import './dark.css'  // AVANT tout - force fond noir
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
